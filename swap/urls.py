080003f4a4050f9bbc951e3e7689376f4216249a
David Jung <d.jung460@gmail.com>
2017-03-09 13:19:33 -0800

    Add template stubs

--- swap/urls.py
+++ swap/urls.py
@@ -1,26 +1,34 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from swap import handlers
+from swap import views
 
 urlpatterns = [
-        url(r'^api/login$', handlers.user),
-        url(r'^api/signup$',handlers.user),
+    # view stuff displays pages
+    url(r'^$',views.index),
+    url(r'^login$',views.login),
+    url(r'^join$',views.join),
+    #url(r'^(?P<user>[0-9a-zA-Z]+)',views.user),
 
-        url(r'^api/equipment/add$', handlers.equipment),
-        url(r'^api/equipment/delete$', handlers.equipment),
-	url(r'^api/equipment/get$', handlers.equipment),
+    # api stuff gets passed off to the handler
+    url(r'^api/auth/login$', handlers.user),
+    url(r'^api/auth/join$',handlers.user),
 
-	url(r'^api/trades/add$', handlers.trade),
-	url(r'^api/trades/delete$', handlers.trade),
-	url(r'^api/trades/get$', handlers.trade),
-        
-	url(r'^api/course/add$', handlers.course),
-        url(r'^api/course/delete$', handlers.course),
-	url(r'^api/course/get$', handlers.course),
-
-	url(r'^api/instructor/add$', handlers.instructor),
-        url(r'^api/instructor/delete$', handlers.instructor),
-	url(r'^api/instructor/get$', handlers.instructor),	
+    url(r'^api/equipment/add$', handlers.equipment),
+    url(r'^api/equipment/delete$', handlers.equipment),
+    url(r'^api/equipment/get$', handlers.equipment),
+    
+    url(r'^api/trades/add$', handlers.trade),
+    url(r'^api/trades/delete$', handlers.trade),
+    url(r'^api/trades/get$', handlers.trade),
+    
+    url(r'^api/course/add$', handlers.course),
+    url(r'^api/course/delete$', handlers.course),
+    url(r'^api/course/get$', handlers.course),
+    
+    url(r'^api/instructor/add$', handlers.instructor),
+    url(r'^api/instructor/delete$', handlers.instructor),
+    url(r'^api/instructor/get$', handlers.instructor),	
 ]
 
 urlpatters = format_suffix_patterns(urlpatterns)
--- swap/views.py
+++ swap/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here
+def index(request):
+    return render(request, 'swap/index.html')
+def login(request):
+    return render(request, 'swap/login.html')
+def join(request):
+    return render(request, 'swap/join.html')
+
