2741ef364703d3002c02dde004e01e1471e271e5
David Jung <d.jung460@gmail.com>
2017-03-10 10:13:04 -0800

    Migrate sqldb to python classes

---
+++ schema.sql
@@ -0,0 +1,104 @@
+CREATE TABLE Student(
+    username VARCHAR(32) NOT NULL,
+year INT NOT NULL,
+faculty CHAR(4) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    phoneNumber CHAR(12) NOT NULL,
+
+PRIMARY KEY (username)
+);
+CREATE TABLE Instructor(
+    username VARCHAR(32) NOT NULL,
+faculty CHAR(4) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    name VARCHAR(128) NOT NULL,
+    phoneNumber CHAR(12) NOT NULL,
+
+PRIMARY KEY (username)
+);
+CREATE TABLE Equipment(
+    equipmentID INT NOT NULL,
+    equipmentName VARCHAR(128) NOT NULL,
+    equipmentType CHAR(3) NOT NULL,
+
+PRIMARY KEY (equipmentID)
+);
+CREATE TABLE Class(
+    faculty CHAR(4) NOT NULL,
+classNum CHAR(4) NOT NULL,
+    term CHAR(7) NOT NULL,
+    instructorUsername VARCHAR(32) NOT NULL,
+
+    PRIMARY KEY(faculty, classNum, term),
+    FOREIGN KEY(instructorUsername)
+        REFERENCES Instructor(username)
+);
+CREATE TABLE ClassRequiresEquipment(
+    faculty CHAR(4) NOT NULL,
+classNum CHAR(4) NOT NULL,
+term CHAR(7) NOT NULL,
+equipmentID INT NOT NULL,
+
+PRIMARY KEY (faculty, classNum, term, equipmentID)
+FOREIGN KEY(faculty, classNum, term)
+    REFERENCES Class (faculty, classNum, term)
+FOREIGN KEY(equipmentID)
+    REFERENCES Equipment(equipmentID)
+);
+CREATE TABLE StudentHasEquipment(
+    username VARCHAR(32) NOT NULL,
+    equipmentID INT NOT NULL,
+quantity INT NOT NULL, 
+
+    PRIMARY KEY (username, equipmentID),
+    FOREIGN KEY(username)
+        REFERENCES Student(username)
+    FOREIGN KEY(equipmentID)
+        REFERENCES Equipment(equipmentID)
+);
+CREATE TABLE StudentTakesClass(
+    username VARCHAR(32) NOT NULL,
+    faculty CHAR(4) NOT NULL,
+classNum CHAR(4) NOT NULL,
+term CHAR(7) NOT NULL,
+
+PRIMARY KEY (username,faculty, classNum, term)
+FOREIGN KEY(faculty, classNum, term)
+    REFERENCES Class(faculty, classNum, term),
+FOREIGN KEY(username)
+    REFERENCES Student(username)
+);
+CREATE TABLE PendingTrade(
+tradeID INT NOT NULL,
+requestUsername VARCHAR(32) NOT NULL,
+    responseUsername VARCHAR(32) NOT NULL,
+    requestEquipID INT NOT NULL,
+responseEquipID INT NOT NULL,
+    dateRequested DATETIME NOT NULL,
+    
+    PRIMARY KEY (tradeID),
+    FOREIGN KEY(requestUsername, requestEquipID)
+        REFERENCES StudentHasEquipment(username, equipmentID),
+FOREIGN KEY(responseUsername, responseEquipID)
+        REFERENCES StudentHasEquipment(username, equipmentID)
+);
+CREATE TABLE ConfirmedTrade(
+    tradeID INT NOT NULL,
+requestUsername VARCHAR(32) NOT NULL,
+    responseUsername VARCHAR(32) NOT NULL,
+    requestEquipID INT NOT NULL,
+reponseEquipID INT NOT NULL,
+    dateConfirmed DATETIME NOT NULL,
+    
+    PRIMARY KEY (tradeID),
+    FOREIGN KEY(requestUsername)
+        REFERENCES Student(username),
+FOREIGN KEY(requestEquipID)
+        REFERENCES Equipment(equipmentID),
+FOREIGN KEY(responseUsername)
+        REFERENCES Student(username),
+FOREIGN KEY(reponseEquipID)
+        REFERENCES Equipment(equipmentID)
+);
+
--- swap/models.py
+++ swap/models.py
@@ -1,3 +1,118 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from __future__ import unicode_literals
+
 from django.db import models
 
-# Create your models here.
+
+class Class(models.Model):
+    faculty = models.CharField(primary_key=True, max_length=4)
+    classnum = models.CharField(db_column='classNum', primary_key=True, max_length=4)  # Field name made lowercase.
+    term = models.CharField(primary_key=True, max_length=7)
+    instructorusername = models.ForeignKey('Instructor', models.DO_NOTHING, db_column='instructorUsername')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Class'
+        unique_together = (('faculty', 'classnum', 'term'),)
+
+
+class ClassRequiresEquipment(models.Model):
+    faculty = models.CharField(primary_key=True, max_length=4)
+    classnum = models.CharField(db_column='classNum', primary_key=True, max_length=4)  # Field name made lowercase.
+    term = models.CharField(primary_key=True, max_length=7)
+    equipmentid = models.IntegerField(db_column='equipmentID', primary_key=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'ClassRequiresEquipment'
+        unique_together = (('faculty', 'classnum', 'term', 'equipmentid'),)
+
+
+class ConfirmedTrade(models.Model):
+    tradeid = models.IntegerField(db_column='tradeID', primary_key=True)  # Field name made lowercase.
+    requestusername = models.ForeignKey('Student', models.DO_NOTHING, db_column='requestUsername')  # Field name made lowercase.
+    responseusername = models.ForeignKey('Student', models.DO_NOTHING, db_column='responseUsername')  # Field name made lowercase.
+    requestequipid = models.ForeignKey('Equipment', models.DO_NOTHING, db_column='requestEquipID')  # Field name made lowercase.
+    reponseequipid = models.ForeignKey('Equipment', models.DO_NOTHING, db_column='reponseEquipID')  # Field name made lowercase.
+    dateconfirmed = models.DateTimeField(db_column='dateConfirmed')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'ConfirmedTrade'
+
+
+class Equipment(models.Model):
+    equipmentid = models.IntegerField(db_column='equipmentID', primary_key=True)  # Field name made lowercase.
+    equipmentname = models.CharField(db_column='equipmentName', max_length=128)  # Field name made lowercase.
+    equipmenttype = models.CharField(db_column='equipmentType', max_length=3)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Equipment'
+
+
+class Instructor(models.Model):
+    username = models.CharField(primary_key=True, max_length=32)
+    faculty = models.CharField(max_length=4)
+    email = models.CharField(max_length=64)
+    name = models.CharField(max_length=128)
+    phonenumber = models.CharField(db_column='phoneNumber', max_length=12)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Instructor'
+
+
+class PendingTrade(models.Model):
+    tradeid = models.IntegerField(db_column='tradeID', primary_key=True)  # Field name made lowercase.
+    requestusername = models.CharField(db_column='requestUsername', max_length=32)  # Field name made lowercase.
+    responseusername = models.CharField(db_column='responseUsername', max_length=32)  # Field name made lowercase.
+    requestequipid = models.IntegerField(db_column='requestEquipID')  # Field name made lowercase.
+    responseequipid = models.IntegerField(db_column='responseEquipID')  # Field name made lowercase.
+    daterequested = models.DateTimeField(db_column='dateRequested')  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'PendingTrade'
+
+
+class Student(models.Model):
+    username = models.CharField(primary_key=True, max_length=32)
+    year = models.IntegerField()
+    faculty = models.CharField(max_length=4)
+    email = models.CharField(max_length=64)
+    name = models.CharField(max_length=128)
+    phonenumber = models.CharField(db_column='phoneNumber', max_length=12)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'Student'
+
+
+class StudentHasEquipment(models.Model):
+    username = models.ForeignKey(Student, models.DO_NOTHING, db_column='username', primary_key=True)
+    equipmentid = models.IntegerField(db_column='equipmentID', primary_key=True)  # Field name made lowercase.
+    quantity = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'StudentHasEquipment'
+        unique_together = (('username', 'equipmentid'),)
+
+
+class StudentTakesClass(models.Model):
+    username = models.ForeignKey(Student, models.DO_NOTHING, db_column='username', primary_key=True)
+    faculty = models.CharField(primary_key=True, max_length=4)
+    classnum = models.CharField(db_column='classNum', primary_key=True, max_length=4)  # Field name made lowercase.
+    term = models.CharField(primary_key=True, max_length=7)
+
+    class Meta:
+        managed = False
+        db_table = 'StudentTakesClass'
+        unique_together = (('username', 'faculty', 'classnum', 'term'),)
